bdeba4146711025ecb235371ae9b8942a0254a46
Sebastian <sebastianploch@protonmail.com>
2020-05-13 21:17:54 +0100

    Implemented basic input handler for Mouse & Keyboard

---
+++ Source/InputState.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+class InputState
+{
+public:
+	InputState(const HWND& window);
+	~InputState();
+
+	void Update();
+	void Reset();
+
+	inline const DirectX::Mouse& GetMouse() const { return *m_mouse; }
+	inline const DirectX::Mouse::ButtonStateTracker& GetMouseState() const { return m_mouseTracker; }
+
+	inline const DirectX::Keyboard& GetKeyboard() const { return *m_keyboard; }
+	inline const DirectX::Keyboard::KeyboardStateTracker& GetKeyboardState() const { return m_keyboardTracker; }
+
+private:
+	std::unique_ptr<DirectX::Mouse>			m_mouse;
+	std::unique_ptr<DirectX::Keyboard>		m_keyboard;
+
+	DirectX::Mouse::ButtonStateTracker		m_mouseTracker;
+	DirectX::Keyboard::KeyboardStateTracker m_keyboardTracker;
+};
